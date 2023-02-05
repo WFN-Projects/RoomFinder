08c2c1213d1b257104eb3fd1751effe86ab2a971
dg022 <dgeorg6@uwo.ca>
2023-02-05 15:38:58 -0500

    configuration

--- server/database.js
+++ server/database.js
@@ -1,23 +1,16 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'my_user',
-  host: 'localhost',
-  database: 'my_database',
-  password: 'root',
-  port: 5432,
-});
+const client = require("./client")
 
 const floorAvail = (time, building, floor) => {
   return new Promise(function(resolve, reject) {
     const {time, building, floor} = body;
-    pool.query("SELECT Availablility FROM Data WHERE Building_Name = '' AND Floor_Number = `${floor}) ")
+    client.query("SELECT Availablility FROM Data WHERE Building_Name = '' AND Floor_Number = `${floor}) ")
   })
 }
 
 const updateRoom = (time, building, floor, room, status) => {
   return new Promise(function(resolve, reject) {
     const {time, building, floor, room, status} = body;
-    pool.query('UPDATE Room_Number (time, building, floor, room) VALUES (status) RETURNING *', [time, building, floor, room, status], (error, results) => {
+    client.query('UPDATE Room_Number (time, building, floor, room) VALUES (status) RETURNING *', [time, building, floor, room, status], (error, results) => {
       if (error) {
         reject(error)
       }
