1bae41fc80f4dd6acc273d4c3f9d54d66991438c
Joy <joyz030707@gmail.com>
2023-03-02 19:55:28 -0500

    remove comment

--- server/client.js
+++ server/client.js
@@ -1,21 +1,10 @@
 var pg = require('pg');
 require('dotenv').config();
-var mysql = require('mysql');
 var db_connection_stirng = process.env.DATABASE_URL;
 var client = new pg.Client(db_connection_stirng);
 
 (async () => {
     await client.connect();
-    
-    /*const building = "ohall";
-    const floor = "2e";
-    const query = "SELECT Availability FROM roomavail WHERE Building_Name = " + mysql.escape(building) + "AND Floor_Number = " + mysql.escape(floor);
-    const response = await client.query(query);
-    const availRooms = [];
-    for (var i=0; i<response.rows.length; i++) {
-      availRooms.push(response.rows[i].availability);
-    }
-    console.log(availRooms);*/
-  })();
+})();
 
 module.exports = client;
