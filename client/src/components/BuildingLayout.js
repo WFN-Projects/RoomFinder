ebcca23066445b5c41f87483a095e1d83dbbdf63
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-03-16 19:24:31 -0400

    fixed spacing between tiles

--- client/src/components/BuildingLayout.js
+++ client/src/components/BuildingLayout.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 
 function BuildingLayout({ building }) {
   const { name } = useParams();
-  const formattedName = name.replace('_', ' ');
+  const formattedName = name.replace("_", " ");
   const floorList = [];
 
   const numberOfFloors = {
@@ -20,39 +20,60 @@ function BuildingLayout({ building }) {
     London_Hall: 6,
   };
 
-  let marginTop = 0;
+  // let marginTop = 0;
   const floorsPerBuilding = numberOfFloors[name];
 
   for (let i = 0; i < floorsPerBuilding; i++) {
-    floorList.push(".")
+    floorList.push(".");
   }
 
-  const colourArray = ['#C7ACFF','#B18CFF','#A07EE7','#8C6CD0','#8266BF', '#6E55A4',
-  '#5C4793','#4A3782', '#3B2D6E', '#2C235A','#1E1846','#110E32','#02061D'];
+  const colourArray = [
+    "#C7ACFF",
+    "#B18CFF",
+    "#A07EE7",
+    "#8C6CD0",
+    "#8266BF",
+    "#6E55A4",
+    "#5C4793",
+    "#4A3782",
+    "#3B2D6E",
+    "#2C235A",
+    "#1E1846",
+    "#110E32",
+    "#02061D",
+  ];
 
-
-  
-return (
-  <div className="background-Col"> 
-    <div>
-      <h1 className="building-Name">{formattedName}</h1>
-      <ul className="building" style={{ listStyle: "none"}}>
-        {floorList.map((floor, i) => (
-          <li key={i}>
-            <div
-              className="floor"
-              style={{
-                top: `${i * 20}px`,
-                background: `linear-gradient(to bottom, ${colourArray[i % colourArray.length]}, #FFFFFF)`,
-              }}
-            ></div>
-          </li>
-        ))}
-      </ul>
+  return (
+    <div className="background-Col">
+      <div>
+        <h1 className="building-Name">{formattedName}</h1>
+        <ul className="building" style={{ listStyle: "none" }}>
+          {floorList.map((floor, i) => (
+            <li key={i} style={{ marginBottom: "100px" }}>
+              <div
+                style={{
+                  top: `${i * 80}px`,
+                  background: `linear-gradient(to bottom, ${
+                    colourArray[i % colourArray.length]
+                  }, #FFFFFF)`,
+                  position: "absolute",
+                  width: 455.31,
+                  height: 261.78,
+                  left: 141.43,
+                  marginTop: 50,
+                  backgroundColor: "#c7acff",
+                  bottom: 50,
+                  borderRadius: 28,
+                  transform: "matrix(0.88, -0.47, 0.88, 0.47, 0, 0)",
+                  marginBottom: 100 /* adjust the value as needed */,
+                }}
+              ></div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
-);
-
+  );
 }
 
 export default BuildingLayout;
