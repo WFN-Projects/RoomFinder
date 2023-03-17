037ade70f3e36414b406ed17b3d145223fd26580
johannn05 <102449221+johannn05@users.noreply.github.com>
2023-03-17 17:12:45 -0400

    remving node modules

--- client/src/components/Building.js
+++ client/src/components/Building.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import ohall from './images/OHall.png'
+
 
 const Building = ({ curr_building }) => {
   return (
     <div>
-      <center> 
-      <img href={`/buildinglayout/${curr_building.name}`} src={ohall} alt={curr_building.name} />   
+      <center>   
       <h1>
         <Link to={`/buildinglayout/${curr_building.name}`}>
           {curr_building.name}
