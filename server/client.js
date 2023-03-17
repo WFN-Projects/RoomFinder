614e1164c0671df1dafb200a1447fb74ce9ca56f
annooee <116043965+annooee@users.noreply.github.com>
2023-03-16 21:54:00 -0400

    done with this

--- server/client.js
+++ server/client.js
@@ -1,10 +1,10 @@
-var pg = require('pg');
-require('dotenv').config();
+var pg = require("pg");
+require("dotenv").config();
 var db_connection_stirng = process.env.DATABASE_URL;
 var client = new pg.Client(db_connection_stirng);
 
 (async () => {
-    await client.connect();
+  await client.connect();
 })();
 
 module.exports = client;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,65 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3001
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3001;
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-const database = require('./database')
+const database = require("./database");
 
-app.get('/roomAvail', (req, res) => {
-    const building = req.body.building;
-    const floor = req.body.floor;
+app.get("/", (req, res) => {
+  console.log("stfu");
+});
 
-    database.roomAvail(building, floor)
-    .then(response => {
-    const obj = Object.fromEntries(response);
-    res.status(200).send(JSON.stringify(obj));
-    })
-    .catch(error => {
-        res.status(500).send(error);
-    })
-})
+app.get("/roomAvail", (req, res) => {
+  const building = req.body.building;
+  const floor = req.body.floor;
 
-app.post('/updateRoom', (req, res) => {
-    const building = req.body.building;
-    const floor = req.body.floor;
-    const room = req.body.room;
-    const status = req.body.status;
-    database.updateRoom(building, floor, room, status)
-    .then(response => {
-        res.status(200).send(response);
+  database
+    .roomAvail(building, floor)
+    .then((response) => {
+      const obj = Object.fromEntries(response);
+      res.status(200).send(JSON.stringify(obj));
     })
-    .catch(error => {
-        res.status(500).send(error);
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/updateRoom", (req, res) => {
+  const building = req.body.building;
+  const floor = req.body.floor;
+  const room = req.body.room;
+  const status = req.body.status;
+  database
+    .updateRoom(building, floor, room, status)
+    .then((response) => {
+      res.status(200).send(response);
     })
-})
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/occupancyProbability", (req, res) => {
+  const room = req.body.room;
+  console.log(room);
+  database
+    .occupancyProbability(room)
+    .then((response) => {
+      const obj = Object.fromEntries(response);
+      res.status(200).send(JSON.stringify(obj));
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
